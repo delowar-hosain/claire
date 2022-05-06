97e3bfcf7557a0985a633a09900e30de2be134ae
Delowar Hossain <40049957+HossainSaide@users.noreply.github.com>
2022-05-06 11:48:43 +0600

    fixing

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,9 +1,9 @@
 export const firebaseConfig = {
-    apiKey: 'AIzaSyAEFgcGxQ3KWyI21v_wFvmgCa-vRBGLysA',
-    authDomain: 'react-firebase-6d101.firebaseapp.com',
-    projectId: 'react-firebase-6d101',
-    storageBucket: 'react-firebase-6d101.appspot.com',
-    messagingSenderId: '863420843982',
-    appId: '1:863420843982:web:77815d120a6c0d31dd0d9f',
-    measurementId: 'G-RQMHWZ8YYT'
+    apiKey: "AIzaSyAEFgcGxQ3KWyI21v_wFvmgCa-vRBGLysA",
+    authDomain: "react-firebase-6d101.firebaseapp.com",
+    projectId: "react-firebase-6d101",
+    storageBucket: "react-firebase-6d101.appspot.com",
+    messagingSenderId: "863420843982",
+    appId: "1:863420843982:web:77815d120a6c0d31dd0d9f",
+    measurementId: "G-RQMHWZ8YYT"
 };
--- src/services/firebase/index.ts
+++ src/services/firebase/index.ts
@@ -3,8 +3,19 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 import "firebase/compat/auth";
 import "firebase/compat/firestore";
-import { firebaseConfig } from "@/config/index";
-import { getDatabase, ref, DataSnapshot} from "firebase/database";
+import { getDatabase, ref, DataSnapshot } from "firebase/database";
+const firebaseConfig = {
+    apiKey: "AIzaSyBuP26lQbWkl5pD66ZrsXZjOtB9Lt47aUQ",
+    authDomain: "claire-dev-87971.firebaseapp.com",
+    databaseURL: "https://claire-dev-87971-default-rtdb.firebaseio.com",
+    projectId: "claire-dev-87971",
+    storageBucket: "claire-dev-87971.appspot.com",
+    messagingSenderId: "790428269592",
+    appId: "1:790428269592:web:2b8feb0fe730168ac3576d",
+    measurementId: "G-94C5TQD588"
+  };
+
+// Initialize Firebase
 const firebaseApp = initializeApp(firebaseConfig);
 
 const auth = getAuth(firebaseApp); // For Authentication

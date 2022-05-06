bf516b7036668af503b0508c2ab38eb6a23223cd
Delowar Hossain <40049957+HossainSaide@users.noreply.github.com>
2022-05-06 10:01:02 +0600

    final commit

--- src/context/authContext.ts
+++
@@ -1,6 +0,0 @@
-import {createContext} from 'react'
-import {userContextState} from '@/interface/index'
-export type AuthContextProps = {
-    user: userContextState;
-} 
-export const AuthContext = createContext<AuthContextProps>({} as AuthContextProps)
--- src/hooks/authHooks.ts
+++
@@ -1,4 +0,0 @@
-import { useContext } from 'react';
-import {AuthContext} from '@/context/authContext';
-
-export const useAuthContext = () => useContext(AuthContext);
